416c813770c7a9cd69eb7fd8fc8362a27fdc16f1
edmi48 <edmi48@gmail.com>
2013-11-04 19:32:04 -0500

    89

--- www/js/data-autocomplete.js
+++ www/js/data-autocomplete.js
@@ -96,7 +96,7 @@ function carga_informacion(sql,objeto,info)
 							html = '<a href="mapa2.html" target="_self"><img src="'+icono+'" height="42" width="42">&nbsp;&nbsp;' + result2.rows.item(j)['programa'] + ' (Ver mapa)</a> <br><strong>' + result2.rows.item(j)['entidad_encargada'] + '</strong> <br>' + result2.rows.item(j)['descripcion_tipo_informacion'];
 						  }
 					      //var thehtml = '<a href="#" onclick=\'abrir_opcion("mapa.html?Entidad='+suggestion.data+'");return false\'><img src="'+icono+'" height="42" width="42">&nbsp;&nbsp;' + suggestion.data + ' (Ver mapa)</a> <br> <strong>Entidad Encargada</strong> ' + suggestion.data2 + ' <br> <strong>Descripción Conflicto:</strong> ' + suggestion.data3;
-						  html = '<a href="mapa.html?Entidad='+result2.rows.item(j)['programa']+'" target="_self"><img src="'+icono+'" height="42" width="42">&nbsp;&nbsp;' + result2.rows.item(j)['programa'] + ' (Ver mapa)</a> <br><strong>' + result2.rows.item(j)['entidad_encargada'] + '</strong> <br>' + result2.rows.item(j)['descripcion_tipo_informacion'];
+						  //html = '<a href="mapa.html?Entidad='+result2.rows.item(j)['programa']+'" target="_self"><img src="'+icono+'" height="42" width="42">&nbsp;&nbsp;' + result2.rows.item(j)['programa'] + ' (Ver mapa)</a> <br><strong>' + result2.rows.item(j)['entidad_encargada'] + '</strong> <br>' + result2.rows.item(j)['descripcion_tipo_informacion'];
 						}
 					   }
 					   $(objeto).html(html);
